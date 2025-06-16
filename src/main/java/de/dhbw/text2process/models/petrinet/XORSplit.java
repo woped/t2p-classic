03886491f7ce64f2346f76c50e1a605d837d7f92
I569996 <maximilian.otto@sap.com>
2025-06-16 14:21:06 +0200

    another one #2

--- src/main/java/de/dhbw/text2process/models/petrinet/XORSplit.java
+++ src/main/java/de/dhbw/text2process/models/petrinet/XORSplit.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class XORSplit extends PetrinetGateway {
 
-  String a, b, c;
+  String a, b, c, d;
   String transID;
   private int choiceCount;
   private String originID;
